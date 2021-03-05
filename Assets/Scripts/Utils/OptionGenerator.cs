79dcfd956a34851bc6cd0d3da951540f84743607
Jecosine <mj_ol@outlook.com>
2021-03-05 22:41:45 +0800

    INIT

---
+++ Assets/Scripts/Utils/OptionGenerator.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System;
+using MicroscopeNamespace.Model;
+
+namespace MicroscopeNamespace.Utils
+{
+  public class OptionGenerator : MonoBehaviour
+  {
+    public Transform uiParent;
+    public GameObject optionTemplate;
+    public void OptionFactory(PartialObject partialObject)
+    {
+      
+    }
+  }
+}
